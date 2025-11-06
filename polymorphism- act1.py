5e7d1b68e196cbe6961d428cf004dab0a1f9c689
vidalmiakoleen-create <vidalmiakoleen@gmail.com>
2025-11-06 09:36:55 +0800

    Create polymorphism- act1.py

---
+++ polymorphism- act1.py
@@ -0,0 +1,11 @@
+class InkPrinter:
+    def print_document(self):
+        return "Printing using ink..."
+
+class LaserPrinter:
+    def print_document(self):
+        return "Printing using laser..."
+
+printers = [InkPrinter(), LaserPrinter()]
+for p in printers:
+    print(p.print_document()
